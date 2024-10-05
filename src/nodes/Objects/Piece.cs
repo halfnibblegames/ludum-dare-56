ea9e92f80365f041967a41b32bfcec98ae01c152
Tom Rijnbeek <tom@tomrijnbeek.me>
2024-10-05 15:22:57 +0200

    🚧 Make piece abstract

--- src/nodes/Objects/Piece.cs
+++ src/nodes/Objects/Piece.cs
@@ -1,15 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Godot;
 
 namespace HalfNibbleGame.Objects;
 
-public sealed class Piece : Node2D
+public abstract class Piece : Node2D
 {
-    public IEnumerable<TileCoord> ReachableTiles(TileCoord currentTile, Board board)
-    {
-        return currentTile.EnumerateValidNeighboring();
-        // return currentTile.EnumerateStepsWhileValid(Step.UpRight)
-        //     .Concat(currentTile.EnumerateStepsWhileValid(Step.DownLeft));
-    }
+    public abstract IEnumerable<TileCoord> ReachableTiles(TileCoord currentTile, Board board);
 }
---
+++ src/nodes/Objects/Pieces/QueenBee.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HalfNibbleGame.Objects.Pieces;
+
+public sealed class QueenBee : Piece
+{
+    public override IEnumerable<TileCoord> ReachableTiles(TileCoord currentTile, Board board)
+    {
+        return currentTile.EnumerateValidNeighboring();
+    }
+}
